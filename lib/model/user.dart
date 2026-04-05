a3b19b00093e274fadfcceb4401e95bc9795d4c1
Jone <jshell97s@yandex.com>
2026-04-05 21:33:25 +0300

    model:create_user_model

--- lib/model/user.dart
+++ lib/model/user.dart
@@ -1,1 +1,17 @@
-class User {}
+class User {
+  final int id;
+  final String name;
+  final String email;
+  final int numbers;
+  final String avatar;
+
+  User({
+    required this.id,
+    required this.name,
+    required this.email,
+    required this.numbers,
+    required this.avatar,
+  });
+
+  
+}
